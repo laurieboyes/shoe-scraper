bc6fa0628b1961c5d96c919e44f11391d9038d57
Laurie Boyes <laurie.boyes@madetech.com>
2022-06-24 11:18:24 +0100

    Initial commit generated with Inteliij

---
+++ src/test/java/uk/co/lrnk/shoescraper/ShoeScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.lrnk.shoescraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
